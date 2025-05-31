2cf39068496eba1863093606403b58b42acc1998
Aman Gupta <itamangupta365@gmail.com>
2025-06-01 01:38:32 +0530

    Updated setup.py version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="gsearchpy",
-    version="0.1.0",
+    version="0.1.1",
     description="A simple Python package for performing Google searches",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
